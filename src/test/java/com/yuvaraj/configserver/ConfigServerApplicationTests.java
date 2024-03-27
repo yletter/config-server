bb94f57f5414d11f01946b54444842a7d98cf2b0
yuvarajengineer <yuvarajengineer@gmail.com>
2024-03-27 20:44:11 +0530

    config server init

---
+++ src/test/java/com/yuvaraj/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuvaraj.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
