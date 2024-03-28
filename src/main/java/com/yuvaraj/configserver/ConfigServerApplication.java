4098f370c93d2e6588b113587895f544906d44ac
yuvarajengineer <yuvarajengineer@gmail.com>
2024-03-28 13:58:54 +0530

    Import

--- src/main/java/com/yuvaraj/configserver/ConfigServerApplication.java
+++ src/main/java/com/yuvaraj/configserver/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.yuvaraj.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
