b5f08b155e78a9fd5283dc183d310d0974d5762c
yuvarajengineer <yuvarajengineer@gmail.com>
2024-03-30 20:20:39 +0530

    All set

--- src/main/java/com/yuvaraj/configserver/ConfigServerApplication.java
+++ src/main/java/com/yuvaraj/configserver/ConfigServerApplication.java
@@ -22,7 +22,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider;
 import com.amazonaws.regions.Regions;
@@ -51,6 +52,11 @@ public class ConfigServerApplication extends SpringBootServletInitializer implem
 
     public static ApplicationContext getApplicationContext() {
         return applicationContext;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Bean
